1104f06e69a1422b938e1d52eda014cd75667e91
none <113509874+azihakim@users.noreply.github.com>
2022-12-28 21:39:45 +0700

    Gabung 1
    Splash screen, landing page, icon launcher

---
+++ app/src/main/java/com/pab/localbrandstr_ina/LandingpageActivity.java
@@ -0,0 +1,26 @@
+package com.pab.localbrandstr_ina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+public class LandingpageActivity extends AppCompatActivity {
+    ImageView img_2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landingpage);
+
+        img_2 = findViewById(R.id.img_2);
+        img_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LandingpageActivity.this,LogoActivity.class));
+            }
+        });
+    }
+}
