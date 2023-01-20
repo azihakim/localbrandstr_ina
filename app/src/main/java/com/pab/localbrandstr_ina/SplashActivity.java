dd4159bfece01280a3e8a42b328433797408ee0b
none <113509874+azihakim@users.noreply.github.com>
2023-01-20 11:56:31 +0700

    Update SplashActivity.java

--- app/src/main/java/com/pab/localbrandstr_ina/SplashActivity.java
+++ app/src/main/java/com/pab/localbrandstr_ina/SplashActivity.java
@@ -24,6 +24,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this,LandingpageActivity.class));
                 finish();
             }
-        },5000);
+        },3000);
     }
 }
